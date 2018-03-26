53d489707fdb36a5a789e4058a12c7633b291a44
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-03-26 19:49:36 +0200

    chore: create react-instantsearch-connectors package

---
+++ packages/react-instantsearch-connectors/src/index.js
@@ -0,0 +1,23 @@
+export { default as connectAutoComplete } from './connectAutoComplete';
+export { default as connectBreadcrumb } from './connectBreadcrumb';
+export { default as connectConfigure } from './connectConfigure';
+export {
+  default as connectCurrentRefinements,
+} from './connectCurrentRefinements';
+export { default as connectHierarchicalMenu } from './connectHierarchicalMenu';
+export { default as connectHighlight } from './connectHighlight';
+export { default as connectHits } from './connectHits';
+export { default as connectHitsPerPage } from './connectHitsPerPage';
+export { default as connectInfiniteHits } from './connectInfiniteHits';
+export { default as connectMenu } from './connectMenu';
+export { default as connectNumericMenu } from './connectNumericMenu';
+export { default as connectPagination } from './connectPagination';
+export { default as connectPoweredBy } from './connectPoweredBy';
+export { default as connectRange } from './connectRange';
+export { default as connectRefinementList } from './connectRefinementList';
+export { default as connectScrollTo } from './connectScrollTo';
+export { default as connectSearchBox } from './connectSearchBox';
+export { default as connectSortBy } from './connectSortBy';
+export { default as connectStateResults } from './connectStateResults';
+export { default as connectStats } from './connectStats';
+export { default as connectToggleRefinement } from './connectToggleRefinement';
---
+++ packages/react-instantsearch-connectors/src/indexUtils.js
@@ -0,0 +1,216 @@
+import { has, omit, get } from 'lodash';
+
+export function getIndex(context) {
+  return context && context.multiIndexContext
+    ? context.multiIndexContext.targetedIndex
+    : context.ais.mainTargetedIndex;
+}
+
+export function getResults(searchResults, context) {
+  if (searchResults.results && !searchResults.results.hits) {
+    return searchResults.results[getIndex(context)]
+      ? searchResults.results[getIndex(context)]
+      : null;
+  } else {
+    return searchResults.results ? searchResults.results : null;
+  }
+}
+
+export function hasMultipleIndex(context) {
+  return context && context.multiIndexContext;
+}
+
+// eslint-disable-next-line max-params
+export function refineValue(
+  searchState,
+  nextRefinement,
+  context,
+  resetPage,
+  namespace
+) {
+  if (hasMultipleIndex(context)) {
+    return namespace
+      ? refineMultiIndexWithNamespace(
+          searchState,
+          nextRefinement,
+          context,
+          resetPage,
+          namespace
+        )
+      : refineMultiIndex(searchState, nextRefinement, context, resetPage);
+  } else {
+    // When we have a multi index page with shared widgets we should also
+    // reset their page to 1 if the resetPage is provided. Otherwise the
+    // indices will always be reset
+    // see: https://github.com/algolia/react-instantsearch/issues/310
+    // see: https://github.com/algolia/react-instantsearch/issues/637
+    if (searchState.indices && resetPage) {
+      Object.keys(searchState.indices).forEach(targetedIndex => {
+        searchState = refineValue(
+          searchState,
+          { page: 1 },
+          { multiIndexContext: { targetedIndex } },
+          true,
+          namespace
+        );
+      });
+    }
+    return namespace
+      ? refineSingleIndexWithNamespace(
+          searchState,
+          nextRefinement,
+          resetPage,
+          namespace
+        )
+      : refineSingleIndex(searchState, nextRefinement, resetPage);
+  }
+}
+
+function refineMultiIndex(searchState, nextRefinement, context, resetPage) {
+  const page = resetPage ? { page: 1 } : undefined;
+  const index = getIndex(context);
+  const state = has(searchState, `indices.${index}`)
+    ? {
+        ...searchState.indices,
+        [index]: { ...searchState.indices[index], ...nextRefinement, ...page },
+      }
+    : {
+        ...searchState.indices,
+        ...{ [index]: { ...nextRefinement, ...page } },
+      };
+  return { ...searchState, indices: state };
+}
+
+function refineSingleIndex(searchState, nextRefinement, resetPage) {
+  const page = resetPage ? { page: 1 } : undefined;
+  return { ...searchState, ...nextRefinement, ...page };
+}
+
+// eslint-disable-next-line max-params
+function refineMultiIndexWithNamespace(
+  searchState,
+  nextRefinement,
+  context,
+  resetPage,
+  namespace
+) {
+  const index = getIndex(context);
+  const page = resetPage ? { page: 1 } : undefined;
+  const state = has(searchState, `indices.${index}`)
+    ? {
+        ...searchState.indices,
+        [index]: {
+          ...searchState.indices[index],
+          ...{
+            [namespace]: {
+              ...searchState.indices[index][namespace],
+              ...nextRefinement,
+            },
+            page: 1,
+          },
+        },
+      }
+    : {
+        ...searchState.indices,
+        ...{ [index]: { [namespace]: nextRefinement, ...page } },
+      };
+  return { ...searchState, indices: state };
+}
+
+function refineSingleIndexWithNamespace(
+  searchState,
+  nextRefinement,
+  resetPage,
+  namespace
+) {
+  const page = resetPage ? { page: 1 } : undefined;
+  return {
+    ...searchState,
+    [namespace]: { ...searchState[namespace], ...nextRefinement },
+    ...page,
+  };
+}
+
+function getNamespaceAndAttributeName(id) {
+  const parts = id.match(/^([^.]*)\.(.*)/);
+  const namespace = parts && parts[1];
+  const attributeName = parts && parts[2];
+
+  return { namespace, attributeName };
+}
+
+// eslint-disable-next-line max-params
+export function getCurrentRefinementValue(
+  props,
+  searchState,
+  context,
+  id,
+  defaultValue,
+  refinementsCallback
+) {
+  const index = getIndex(context);
+  const { namespace, attributeName } = getNamespaceAndAttributeName(id);
+  const refinements =
+    (hasMultipleIndex(context) &&
+      searchState.indices &&
+      namespace &&
+      searchState.indices[`${index}`] &&
+      has(searchState.indices[`${index}`][namespace], `${attributeName}`)) ||
+    (hasMultipleIndex(context) &&
+      searchState.indices &&
+      has(searchState, `indices.${index}.${id}`)) ||
+    (!hasMultipleIndex(context) &&
+      namespace &&
+      has(searchState[namespace], attributeName)) ||
+    (!hasMultipleIndex(context) && has(searchState, id));
+  if (refinements) {
+    let currentRefinement;
+
+    if (hasMultipleIndex(context)) {
+      currentRefinement = namespace
+        ? get(searchState.indices[`${index}`][namespace], attributeName)
+        : get(searchState.indices[index], id);
+    } else {
+      currentRefinement = namespace
+        ? get(searchState[namespace], attributeName)
+        : get(searchState, id);
+    }
+
+    return refinementsCallback(currentRefinement);
+  }
+
+  if (props.defaultRefinement) {
+    return props.defaultRefinement;
+  }
+
+  return defaultValue;
+}
+
+export function cleanUpValue(searchState, context, id) {
+  const index = getIndex(context);
+  const { namespace, attributeName } = getNamespaceAndAttributeName(id);
+  if (hasMultipleIndex(context) && Boolean(searchState.indices)) {
+    return namespace
+      ? {
+          ...searchState,
+          indices: {
+            ...searchState.indices,
+            [index]: {
+              ...searchState.indices[index],
+              [namespace]: omit(
+                searchState.indices[index][namespace],
+                `${attributeName}`
+              ),
+            },
+          },
+        }
+      : omit(searchState, `indices.${index}.${id}`);
+  } else {
+    return namespace
+      ? {
+          ...searchState,
+          [namespace]: omit(searchState[namespace], `${attributeName}`),
+        }
+      : omit(searchState, `${id}`);
+  }
+}
