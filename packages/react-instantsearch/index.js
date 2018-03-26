c8b062410bdb8a900c46ba0206c0c5e806aae3d1
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-03-26 16:27:16 +0200

    chore: remove main package

--- packages/react-instantsearch/index.js
+++
@@ -1,1 +0,0 @@
-export { default as createConnector } from './src/core/createConnector';
--- packages/react-instantsearch/server.js
+++
@@ -1,8 +0,0 @@
-import { createInstantSearch } from './src/core/createInstantSearchServer';
-import algoliasearch from 'algoliasearch/lite';
-
-const cis = function() {
-  return createInstantSearch(algoliasearch);
-};
-
-export { cis as createInstantSearch };
