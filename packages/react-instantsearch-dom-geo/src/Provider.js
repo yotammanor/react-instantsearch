991de2b538310fda0f2601057083ea317a729ec2
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-04-26 10:20:29 +0200

    feat(Provider): handle hasMapMoveSinceLastRefine

--- packages/react-instantsearch-dom-geo/src/Provider.js
+++ packages/react-instantsearch-dom-geo/src/Provider.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import { connectGeoSearch } from '../../react-instantsearch/connectors';
 import { LatLngPropType, BoundingBoxPropType } from './propTypes';
 
+export const STATE_CONTEXT = '__ais_geo_search__state__';
+
 export class Provider extends Component {
   static propTypes = {
     hits: PropTypes.arrayOf(PropTypes.object).isRequired,
@@ -13,6 +15,40 @@ export class Provider extends Component {
     children: PropTypes.func.isRequired,
     position: LatLngPropType,
     currentRefinement: BoundingBoxPropType,
+  };
+
+  static childContextTypes = {
+    [STATE_CONTEXT]: PropTypes.shape({
+      hasMapMoveSinceLastRefine: PropTypes.bool.isRequired,
+      setMapMoveSinceLastRefine: PropTypes.func.isRequired,
+    }).isRequired,
+  };
+
+  state = {
+    hasMapMoveSinceLastRefine: false,
+  };
+
+  getChildContext() {
+    const { hasMapMoveSinceLastRefine } = this.state;
+
+    return {
+      [STATE_CONTEXT]: {
+        setMapMoveSinceLastRefine: this.setMapMoveSinceLastRefine,
+        hasMapMoveSinceLastRefine,
+      },
+    };
+  }
+
+  setMapMoveSinceLastRefine = next => {
+    const { hasMapMoveSinceLastRefine } = this.state;
+
+    if (hasMapMoveSinceLastRefine === next) {
+      return;
+    }
+
+    this.setState(() => ({
+      hasMapMoveSinceLastRefine: next,
+    }));
   };
 
   render() {
@@ -25,9 +61,13 @@ export class Provider extends Component {
       children,
     } = this.props;
 
+    const { hasMapMoveSinceLastRefine } = this.state;
+
     return children({
+      setMapMoveSinceLastRefine: this.setMapMoveSinceLastRefine,
       hits,
       isRefinedWithMap,
+      hasMapMoveSinceLastRefine,
       position,
       currentRefinement,
       refine,
